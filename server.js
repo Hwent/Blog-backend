242062bd16c0b80f1d3336f9d528ab459f96d82d
Wentao <wentaowilliam@outlook.com>
2024-03-14 23:23:54 -0400

    Connect to mongodb

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
 const express = require("express");
 const app = express();
 const Post = require("./models/Post");
+app.use(express.json());
+
+// Connect to MongoDB
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI);
+
 app.get("/", (req, res) => {
-  res.direct("/posts");
+  res.redirect("/posts");
 });
 
 app.get("/posts", async (req, res) => {
-  await Post.find({}, (err, posts) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json({ posts });
+  const posts = await Post.find({});
+  res.json(posts);
+});
+
+app.post("/posts", async (req, res) => {
+  const post = new Post(req.body);
+  await post.save();
+  res.json(post);
+});
+
+app.get("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.json(post);
+});
+
+app.put("/posts/:id", async (req, res) => {
+  const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
   });
+  res.json(post);
+});
+
+app.delete("/posts/:id", async (req, res) => {
+  const post = await Post.findByIdAndDelete(req.params.id);
+  res.json(post);
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
